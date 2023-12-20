5ce26b648f770b79b680ce29f15b41d971ae6c86
Yuri Batan <batanyuri@gmail.com>
2023-12-20 07:28:03 -0600

    Created Menu File and added skeleton

---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+function Menu() {
+    return (
+        <div className="App">
+        <h1> Hello this is my first application</h1>
+        </div>
+    );
+      
+     
+}
+
+export default Menu;
