084fa3df854d63adadead9c5ae74a98d682f3ff7
Yuri Batan <batanyuri@gmail.com>
2023-12-20 07:48:06 -0600

    Created Boilerplate for Credits

---
+++ src/Game Functions/Credits/Credits.js
@@ -0,0 +1,19 @@
+function Credit () {
+    return (
+        <div className = "Credits">
+            <div className = "Title">
+                <h1>Credits</h1>
+            </div>
+            
+            <div className = "Credits">
+                <h2>Created by Yuri Batan</h2>
+            </div>
+
+            <div className = "returnToMenu">
+                <button> Return to Menu </button>
+            </div>
+        </div>
+    )
+}
+
+export default Credit
